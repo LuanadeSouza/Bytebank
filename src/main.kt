bbe281800b8cf6119c7b22eb6213e851ee814b08
Luana Dev <luana@wezo.com.br>
2020-08-21 08:05:33 -0300

    estrutura de repeticao

--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,14 @@
-fun main(){
+fun main() {
+
+    for (i in 1..5) {
+
+    }
 
     //var multavel, val imutavel
     val titular: String = "Luana"
     val numeroConta: Int = 1000
     var saldo = 0.0
-//    saldo = 100.0 + 2.0
-//    saldo += 200
-//    saldo -= 1000
-
+   
     println("Titular $titular")
     println("Número da conta:  $numeroConta")
     println("Saldo da conta: $saldo")
@@ -17,12 +18,12 @@ fun main(){
 
 }
 
-fun testaCondicoes(saldo: Double){
-    if (saldo > 0.0){
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
         println("Conta com saldo: $saldo")
-    }else if (saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("Conta com saldo: $saldo")
-    }else{
+    } else {
         println("Conta com saldo: $saldo.")
     }
 
