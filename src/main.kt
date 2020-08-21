6830ac28f08ee23563769fad167c4dba6f66c397
Luana Dev <luana@wezo.com.br>
2020-08-21 08:17:42 -0300

    "for"

--- src/main.kt
+++ src/main.kt
@@ -1,36 +1,35 @@
 fun main() {
 
-    for (i in 1..5) {
+    for (i in 5..1) {
 
-    }
+        val titular: String = "Luana $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-    //var multavel, val imutavel
-    val titular: String = "Luana"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
-   
-    println("Titular $titular")
-    println("Número da conta:  $numeroConta")
-    println("Saldo da conta: $saldo")
+        println("Titular $titular")
+        println("Número da conta:  $numeroConta")
+        println("Saldo da conta: $saldo")
+        println()
 
 
 //   testaCondicoes(saldo)
 
-}
-
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0) {
-        println("Conta com saldo: $saldo")
-    } else if (saldo == 0.0) {
-        println("Conta com saldo: $saldo")
-    } else {
-        println("Conta com saldo: $saldo.")
     }
 
-    when {
-        saldo > 0.0 -> println("Conta com saldo: $saldo ")
-        saldo == 0.0 -> println("Conta com saldo: $saldo")
-        else -> println("Conta com saldo: $saldo")
-    }
+    fun testaCondicoes(saldo: Double) {
+        if (saldo > 0.0) {
+            println("Conta com saldo: $saldo")
+        } else if (saldo == 0.0) {
+            println("Conta com saldo: $saldo")
+        } else {
+            println("Conta com saldo: $saldo.")
+        }
 
+        when {
+            saldo > 0.0 -> println("Conta com saldo: $saldo ")
+            saldo == 0.0 -> println("Conta com saldo: $saldo")
+            else -> println("Conta com saldo: $saldo")
+        }
+
+    }
 }
