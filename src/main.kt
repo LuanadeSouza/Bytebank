5fec006c7a8e9e725136f3bd2ba47cf5545a430c
Luana Dev <luana@wezo.com.br>
2020-09-04 14:31:42 -0300

    "Cópia e referência"

--- src/main.kt
+++ src/main.kt
@@ -5,19 +5,37 @@ fun main() {
     contaAlex.titular = "Alex"
     contaAlex.numero = 1000
     contaAlex.saldo = 200.0
-    println(contaAlex.titular)
-    println(contaAlex.numero)
-    println(contaAlex.saldo)
+//    println(contaAlex.titular)
+//    println(contaAlex.numero)
+//    println(contaAlex.saldo)
 
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
     contaFran.saldo = 300.0
-    println(contaFran.titular)
-    println(contaFran.numero)
-    println(contaFran.saldo)
+//    println(contaFran.titular)
+//    println(contaFran.numero)
+//    println(contaFran.saldo)
 
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
 }
 
 class Conta {
