21a7839edfefb02119865342d521d8dc789f4770
Luana Dev <luana@wezo.com.br>
2020-09-11 09:24:37 -0300

    "Criando novas classes"

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,16 @@
 fun main() {
 
     println("Bem vindo ao ByteBank")
+    val Alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
 
+    println("Nome ${Alex.nome}")
+    println("Cpf ${Alex.cpf}")
+    println("Salário ${Alex.salario}")
+    println("Bonificação ${Alex.bonificacao()}")
 }
 
 
