2e141a8d81de5a7d30bb9e5d78ab896c742e17b7
LuanaSouza <luana.souza@ifood.com.br>
2020-09-25 10:09:52 -0300

    reutilizando e sobreescrevendo metodos de Any

--- src/br/com/luanadev/bytebank/main.kt
+++ src/br/com/luanadev/bytebank/main.kt
@@ -1,10 +1,11 @@
 package br.com.luanadev.bytebank
 
+import br.com.luanadev.bytebank.teste.testaAny
 import br.com.luanadev.bytebank.teste.testaAutenticacaoTiposDiferentes
 
 fun main() {
 
-    testaAutenticacaoTiposDiferentes()
+    testaAny()
 
 
 }
--- src/br/com/luanadev/bytebank/teste/testaAny.kt
+++ src/br/com/luanadev/bytebank/teste/testaAny.kt
@@ -3,15 +3,22 @@ package br.com.luanadev.bytebank.teste
 import br.com.luanadev.bytebank.modelo.cliente.Endereco
 
 fun testaAny(){
-    val endereco = Endereco()
-
-    val objeto: Any = Any()
-
-    imprime(Any())
-    imprime(1)
-    imprime(1.0)
-    val teste = imprime(endereco)
-    println(teste)
+    val endereco = Endereco(
+        logradouro = "Rua Vergueiro",
+        complemento = "Alura",
+        cep = "00000-000"
+    )
+    val enderecoNovo = Endereco(
+        bairro = "Vila Mariana",
+        numero = 1000,
+        cep = "00000-000"
+    )
+    println(endereco.equals(enderecoNovo))
+    println(endereco.equals(endereco))
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+    println(endereco.toString())
+    println(enderecoNovo.toString())
 
 
 }
