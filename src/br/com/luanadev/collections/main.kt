83810db977ae26be386ee940152850b28ba15fdd
LuanaSouza <luana.souza@ifood.com.br>
2020-09-30 16:45:02 -0300

    Funcoes de agregacao

--- src/br/com/luanadev/collections/main.kt
+++ src/br/com/luanadev/collections/main.kt
@@ -2,8 +2,8 @@ package br.com.luanadev.collections
 
 fun main() {
 
-    val serie = 1.rangeTo(10)
 }
+
 
 
 
---
+++ src/br/com/luanadev/collections/testaRanger.kt
@@ -0,0 +1,39 @@
+package br.com.luanadev.collections
+
+fun testaRanger() {
+    val serie = 1.rangeTo(10)
+    for (s in serie) {
+        print("$s ")
+    }
+    println()
+
+    val numerosPares = 0..100 step 2
+    for (numeroPar in numerosPares) {
+        print("$numeroPar ")
+    }
+    println()
+
+
+    val outrosnumerosPares = 0.until(100) step 2
+    for (numerosPar in numerosPares) {
+        print("$numerosPar ")
+    }
+    println()
+
+    val numerosParesReverso = 100 downTo 0 step 2
+    numerosParesReverso.forEach { print("$it ") }
+    println()
+
+    val intervalo = 1500..5000
+    val salario = 5001
+    if (salario in intervalo) {
+        println("Está dentro do intervalo")
+    } else {
+        println("Não está dentro do intervalo")
+    }
+    println()
+
+    val alfabeto = 'a'..'z'
+    val letra = 'k'
+    println(letra in alfabeto)
+}
