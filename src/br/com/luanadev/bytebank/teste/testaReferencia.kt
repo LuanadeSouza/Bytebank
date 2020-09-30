787edfe576f2720a997333fd73cba862221d1267
LuanaSouza <luana.souza@ifood.com.br>
2020-09-30 15:03:23 -0300

    Lidando com referencias nulas

---
+++ src/br/com/luanadev/bytebank/teste/testaReferencia.kt
@@ -0,0 +1,22 @@
+package br.com.luanadev.bytebank.teste
+
+import br.com.luanadev.bytebank.modelo.cliente.Endereco
+import java.lang.IllegalArgumentException
+
+fun testaReferencia(){
+
+    var enderecoNulo: Endereco? = Endereco(logradouro = "rua vergueito", complemento = "predio")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalArgumentException("Preencha o campo complemento!")
+        println(tamanhoComplemento)
+    }
+    testes("")
+    testes(1)
+}
+
+fun testes(valor: Any){
+    val numero: Int? = valor as? Int
+    println(numero)
+}
