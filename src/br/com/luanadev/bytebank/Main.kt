1c7066aff09c61449ffa4aa61a8b48d4d294b5a1
LuanaSouza <luana.souza@ifood.com.br>
2020-10-02 13:56:07 -0300

    conhecendo as Collections

---
+++ src/br/com/luanadev/bytebank/Main.kt
@@ -0,0 +1,24 @@
+package br.com.luanadev.bytebank
+
+import br.com.luanadev.collections.array.testaCollection
+
+fun main() {
+
+    testaCollection()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/br/com/luanadev/collections/array/testaCollection.kt
@@ -0,0 +1,29 @@
+package br.com.luanadev.collections.array
+
+fun testaCollection(){
+    testaCopia()
+
+}
+
+fun testaCopia() {
+    val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
+    banco.salva("Alex")
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes{
+
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String){
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+}
+
