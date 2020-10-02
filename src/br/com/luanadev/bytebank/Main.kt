b3507253045964eb68918ea1ebdbe03cd60ed2cf
LuanaSouza <luana.souza@ifood.com.br>
2020-10-02 15:04:56 -0300

    Collection Set

--- src/br/com/luanadev/bytebank/Main.kt
+++ src/br/com/luanadev/bytebank/Main.kt
@@ -1,10 +1,30 @@
 package br.com.luanadev.bytebank
 
-import br.com.luanadev.collections.array.testaCollection
-
 fun main() {
 
-    testaCollection()
+    val assistiramCursoAndroid = mutableSetOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = mutableSetOf("Alex", "Paulo", "Maria")
+    //val assistiramAmbos=  assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos  = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+
+    println(assistiramAmbos)
+
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid union assistiramCursoKotlin)
+
+    println(assistiramCursoAndroid - assistiramCursoKotlin)
+    println(assistiramCursoAndroid subtract  assistiramCursoKotlin)
+
+    println(assistiramCursoAndroid intersect   assistiramCursoKotlin)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
+
 
 }
 
